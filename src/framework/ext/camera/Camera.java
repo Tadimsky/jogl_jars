7ca14162b0a6ebe4eccc59495b4a37c3bfe4d5d5
Jonno Schmidt <jonno@schmidtfam.us>
2015-03-04 22:41:28 -0500

    Fixed up Camera

--- src/framework/ext/camera/Camera.java
+++ src/framework/ext/camera/Camera.java
@@ -1,5 +1,8 @@
 package framework.ext.camera;
 
+import framework.ext.math.Point;
+import framework.ext.math.Vector;
+
 import javax.media.opengl.GL2;
 import javax.media.opengl.glu.GLU;
 
@@ -7,38 +10,45 @@ import javax.media.opengl.glu.GLU;
  * Created by Jonno on 2/8/2015.
  */
 public class Camera {
-    private float[] myPosition;
-    private float[] myLookAt;
-    private float[] myUp;
-    
+    private Point myPosition;
+    private Point myLookAt;
+    private Vector myUp;
+
     private float myXRotation;
     private float myYRotation;
 
     public Camera() {
-        myPosition = new float[] {0f, 1f, 40f};
-        myLookAt = new float[] {0,0,0};
-        myUp = new float[] {0,1,0};
-        myXRotation = 0;
+        this(new Point(0, 1, 40), new Point(0, 0, 0), new Vector(0, 1, 0));        
     }
     
+    public Camera(Point position, Point lookAt, Vector up) {
+        myPosition = new Point(position);
+        myLookAt = new Point(lookAt);
+        
+        myUp = new Vector(up);
+        myUp.normalize();
+    }
+
     public void apply(GL2 gl, GLU glu) {
-        glu.gluLookAt(myPosition[0], myPosition[1], myPosition[2],
-                myLookAt[0], myLookAt[1], myLookAt[2],
-                myUp[0], myUp[1], myUp[2]);
+        glu.gluLookAt(myPosition.x, myPosition.y, myPosition.z,
+                myLookAt.x, myLookAt.y, myLookAt.z,
+                myUp.x, myUp.y, myUp.z);
+        
+        gl.glMatrixRotatedEXT();
 
         gl.glRotatef(myYRotation, 1f, 0f, 0f);
         gl.glRotatef(myXRotation, 0f, 1f, 0f);
     }
-    
+
     public void turn(float xDegrees, float yDegrees) {
         myXRotation += xDegrees;
         myYRotation += yDegrees;
     }
-    
+
     public void moveForward(float amnt) {
         myPosition[2] -= amnt;
     }
-    
+
     public void pan(float x, float y) {
         myPosition[0] += x;
         myPosition[1] += y;
@@ -46,5 +56,5 @@ public class Camera {
         myLookAt[0] += x;
         myLookAt[1] += y;
     }
-    
+
 }
